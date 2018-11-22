4d5d92324cb68d9bb694e02f4a56e4882aa7fa3c
Javier <jafalca>
2018-11-21 23:56:58 -0500

    first commit

---
+++ src/SerialTest.java
@@ -0,0 +1,182 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+import java.util.Enumeration;
+
+
+/**
+ * The following code is to interface the Arduino console output with Java
+ */
+public class SerialTest implements SerialPortEventListener {
+    SerialPort serialPort;
+    /** The port we're normally going to use. */
+    private static final String PORT_NAMES[] = {
+            "/dev/tty.usbserial-A9007UX1", // Mac OS X
+            "/dev/ttyACM0", // Raspberry Pi
+            "/dev/ttyUSB0", // Linux
+            "COM3", // Windows
+    };
+    /**
+     * A BufferedReader which will be fed by a InputStreamReader
+     * converting the bytes into characters
+     * making the displayed results codepage independent
+     */
+    private BufferedReader input;
+    /** The output stream to the port */
+    private OutputStream output;
+    /** Milliseconds to block while waiting for port open */
+    private static final int TIME_OUT = 2000;
+    /** Default bits per second for COM port. */
+    private static final int DATA_RATE = 9600;
+
+    public void initialize() {
+        // the next line is for Raspberry Pi and
+        // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
+        //System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
+
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+        //First, Find an instance of serial port as set in PORT_NAMES.
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            for (String portName : PORT_NAMES) {
+                if (currPortId.getName().equals(portName)) {
+                    portId = currPortId;
+                    break;
+                }
+            }
+        }
+        if (portId == null) {
+            System.out.println("Could not find COM port.");
+            return;
+        }
+
+        try {
+            // open serial port, and use class name for the appName.
+            serialPort = (SerialPort) portId.open(this.getClass().getName(),
+                    TIME_OUT);
+
+            // set port parameters
+            serialPort.setSerialPortParams(DATA_RATE,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+
+            // open the streams
+            input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+            output = serialPort.getOutputStream();
+
+            // add event listeners
+            serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    /**
+     * This should be called when you stop using the port.
+     * This will prevent port locking on platforms like Linux.
+     */
+    public synchronized void close() {
+        if (serialPort != null) {
+            serialPort.removeEventListener();
+            serialPort.close();
+        }
+    }
+
+    /**
+     * Handle an event on the serial port. Read the data and print it.
+     */
+    public synchronized void serialEvent(SerialPortEvent oEvent) {
+        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+                String inputLine=input.readLine();
+                playSound(inputLine);
+                //System.out.println(inputLine);
+            } catch (Exception e) {
+                System.err.println(e.toString());
+            }
+        }
+        // Ignore all the other eventTypes, but you should consider the other ones.
+    }
+
+    public static void main(String[] args) throws Exception {
+        SerialTest main = new SerialTest();
+        main.initialize();
+        Thread t=new Thread() {
+            public void run() {
+                //the following line will keep this app alive for 1000 seconds,
+                //waiting for events to occur and responding to them (printing incoming messages to console).
+                try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
+            }
+        };
+        t.start();
+        System.out.println("Started");
+    }
+
+
+
+
+    public static void playSound(String pin){
+        Synth sound = new Synth();
+        String[] notes = sound.getPianoNotes();
+        switch (pin){
+            case "0 touched":
+                sound.playSound(notes[0]);
+                System.out.println("A");
+                break;
+            case "1 touched":
+                sound.playSound(notes[1]);
+                System.out.println("B");
+                break;
+            case "2 touched":
+                sound.playSound(notes[2]);
+                System.out.println("C");
+                break;
+            case "3 touched":
+                sound.playSound(notes[3]);
+                System.out.println("D");
+                break;
+            case "4 touched":
+                sound.playSound(notes[4]);
+                System.out.println("E");
+                break;
+            case "5 touched":
+                sound.playSound(notes[5]);
+                System.out.println("F");
+                break;
+            case "6 touched":
+                sound.playSound(notes[6]);
+                System.out.println("G");
+                break;
+            case "7 touched":
+                sound.playSound(notes[7]);
+                System.out.println("H");
+                break;
+            case "8 touched":
+                sound.playSound(notes[8]);
+                System.out.println("I");
+                break;
+            case "9 touched":
+                sound.playSound(notes[9]);
+                System.out.println("J");
+                break;
+            case "10 touched":
+                sound.playSound(notes[10]);
+                System.out.println("K");
+                break;
+            case "11 touched":
+                sound.playSound(notes[11]);
+                System.out.println("L");
+                break;
+        }
+
+    }
+}
+
---
+++ src/Synth.java
@@ -0,0 +1,71 @@
+import sun.audio.AudioStream;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.swing.*;
+import javax.sound.sampled.*;
+
+public class Synth {
+
+    /**
+     * @param filename the .wav file containing sound to be played
+     *
+     */
+    public void playSound(String filename){
+
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream((new File(filename).getAbsoluteFile())));
+            clip.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    /**
+     * Instrument set for Piano
+     * */
+    public String[] getPianoNotes(){
+        String[] notes = new String[12];
+        notes[0] = "piano/D3.wav";
+        notes[1] = "piano/F3.wav";
+        notes[2] = "piano/A3.wav";
+        notes[3] = "piano/C4.wav";
+        notes[4] = "piano/D4.wav";
+        notes[5] = "piano/F4.wav";
+        notes[6] = "piano/A4.wav";
+        notes[7] = "piano/C5.wav";
+        notes[8] = "piano/D5.wav";
+        notes[9] = "piano/F5.wav";
+        notes[10] = "piano/A5.wav";
+        notes[11] = "piano/C6.wav";
+
+        return notes;
+
+    }
+    /**
+     * Instrument set for acousticDrum
+     * */
+    public String[] getDrumNotes(){
+        String[] notes = new String[12];
+        notes[0] = "drumAcoustic/BD.wav";
+        notes[1] = "drumAcoustic/CB.wav";
+        notes[2] = "drumAcoustic/CB3.wav";
+        notes[3] = "drumAcoustic/CC.wav";
+        notes[4] = "drumAcoustic/CHH.wav";
+        notes[5] = "drumAcoustic/CS.wav";
+        notes[6] = "drumAcoustic/F.wav";
+        notes[7] = "drumAcoustic/FD2.wav";
+        notes[8] = "drumAcoustic/KC.wav";
+        notes[9] = "drumAcoustic/RM.wav";
+        notes[10] = "drumAcoustic/SD.wav";
+        notes[11] = "drumAcoustic/H.wav";
+
+
+
+        return notes;
+    }
+
+}
